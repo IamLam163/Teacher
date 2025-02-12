0d445cfda4849eacb8b5298a36a4579684af84b9
IamLam163 <olamidealiyu@gmail.com>
2025-02-12 10:45:19 +0100

    md

--- Class/Day_7/me.js
+++ Class/Day_7/me.js
@@ -1,21 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector(".top-banner form");
+  const input = document.querySelector(".top-banner input");
+  const msg = document.querySelector(".top-banner .msg");
+  const list = document.querySelector(".ajax-section .cities");
+  const apiKey = "4d8fb5b93d4af21d66a2948710284366";
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let inputVal = input.value;
+
+    const listItems = list.querySelectorAll(".ajax-section .city");
+  });
+});
+
 /*
-    document.addEventListener("DOMContentLoaded", () => {
-    const form = document.querySelector(".top-banner form");
-    const input = document.querySelector(".top-banner input");
-    const msg = document.querySelector(".top-banner .msg");
-    const list = document.querySelector(".ajax-section .cities");
-    const apiKey = "4d8fb5b93d4af21d66a2948710284366";
-
-    form.addEventListener("submit", (e) => {
-        e.preventDefault();
-        let inputVal = input.value;
-
-        const listItems = list.querySelectorAll(".ajax-section .city");
-    });
-    });
-    */
-
-//const { response } = require("express");
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector(".top-banner form");
   const apiKey = "4d8fb5b93d4af21d66a2948710284366";
@@ -36,3 +34,4 @@ document.addEventListener("DOMContentLoaded", () => {
       });
   });
 });
+*/
